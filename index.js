744b7529b1e8215ba29235e689e6cf6f0f3fbd2d
kaushalmeena <kaushal.meena1996@gmail.com>
2022-06-20 23:58:47 +0530

    refactored code

--- index.js
+++ index.js
@@ -1,81 +1,20 @@
-onDocumentReady(function () {
-  const searchInput = document.getElementById("searchInput");
-  const searchButton = document.getElementById("searchButton");
-  const searchContent = document.getElementById("searchContent");
-
-  searchInput.addEventListener("keypress", function (event) {
-    if (event.key === "Enter") {
-      fetchAnimeList({ searchInput, searchButton, searchContent });
-      return false;
-    }
-  });
-
-  searchButton.addEventListener("click", function () {
-    fetchAnimeList({ searchInput, searchButton, searchContent });
-  });
-});
-
-function fetchAnimeList({ searchInput, searchButton, searchContent }) {
-  const xmlhttp = new XMLHttpRequest();
-  const apiUrl = "https://api.jikan.moe/v3/search/anime?limit=20&q=";
-
-  searchInput.disabled = true;
-  searchButton.disabled = true;
-  searchContent.innerHTML =
-    `<div class="center">
-      <img alt="Loader" class="loader-image" src="./assets/images/loader.gif" />
-    </div>`;
-
-  xmlhttp.onload = function () {
-    if (xmlhttp.status == 200) {
-      searchContent.innerHTML = generateContent(xmlhttp.response.results);
-    } else {
-      searchContent.innerHTML =
-        `<div class="center">
-          Error ${xmlhttp.status}: ${xmlhttp.statusText}
-        </div>`;
-    }
-    searchInput.disabled = false;
-    searchButton.disabled = false;
-  };
-
-  xmlhttp.onerror = function () {
-    searchContent.innerHTML =
-      `<div class="center">
-        Request failed...
-      </div>`;
-  };
-
-  xmlhttp.open("GET", apiUrl + searchInput.value, true);
-  xmlhttp.responseType = "json";
-  xmlhttp.send();
-}
-
-function generateContent(results = []) {
+function generateListHTML(data = []) {
   let html = "";
 
-  let url;
-  let imageUrl;
-  let title;
-  let date;
-  let episodes;
-  let info;
-  let synopsis;
-
-  if (results.length > 0) {
-    for (let i = 0; i < results.length; i++) {
-      url = decodeURIComponent(results[i].url);
-      imageUrl = decodeURIComponent(results[i].image_url);
-      title = results[i].title;
-      date = getDate(results[i].start_date);
-      episodes = results[i].episodes;
-      info = `${results[i].type} - ${episodes} ${episodes > 1 ? "Episodes" : "Episode"}`;
-      synopsis = results[i].synopsis;
+  if (data.length > 0) {
+    for (let i = 0; i < data.length; i += 1) {
+      const url = data[i].url;
+      const imageURL = data[i].images.jpg.image_url;
+      const title = data[i].title;
+      const date = data[i].aired.string;
+      const episodes = data[i].episodes;
+      const info = `${data[i].type} - ${episodes} ` + episodes > 1 ? "Episodes" : "Episode";
+      const synopsis = data[i].synopsis;
 
       html +=
-        `<a target="_blank" rel="noopener noreferrer" href="${url}">
+          `<a target="_blank" href="${url}">
             <div class="card">
-              <img alt="Search" class="card-image" src="${imageUrl}" />
+              <img alt="Anime" class="card-image" src="${imageURL}" />
               <div class="card-content">
                 <div class="card-title">${title}</div>
                 <div class="card-date">${date}</div>
@@ -95,15 +34,53 @@ function generateContent(results = []) {
   return html;
 }
 
-function getDate(dateString) {
-  const date = new Date(dateString);
-  return date.getFullYear();
-}
-
 function onDocumentReady(fn) {
-  if (document.readyState != "loading") {
+  if (document.readyState !== "loading") {
     fn();
   } else {
     document.addEventListener("DOMContentLoaded", fn);
   }
 }
+
+onDocumentReady(() => {
+  const searchInput = document.getElementById("searchInput");
+  const searchButton = document.getElementById("searchButton");
+  const searchContent = document.getElementById("searchContent");
+
+  function fetchAnime() {
+    const API_URL = `https://api.jikan.moe/v4/anime?limit=20&q=${searchInput.value}`;
+
+    searchInput.disabled = true;
+    searchButton.disabled = true;
+    searchContent.innerHTML = 
+      `<div class="center">
+        <img alt="Loader" class="loader-image" src="./assets/images/loader.gif" />
+      </div>`;
+
+    fetch(API_URL)
+      .then((response) => response.json())
+      .then((response) => {
+        searchContent.innerHTML = generateListHTML(response.data);
+      })
+      .catch(() => {
+        searchContent.innerHTML = 
+          `<div class="center">
+            Request failed...
+          </div>`;
+      })
+      .finally(() => {
+        searchInput.disabled = false;
+        searchButton.disabled = false;
+      });
+  }
+
+  searchInput.addEventListener("keypress", (event) => {
+    if (event.key === "Enter") {
+      fetchAnime();
+    }
+  });
+
+  searchButton.addEventListener("click", () => {
+    fetchAnime();
+  });
+});
